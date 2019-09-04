a1e7ea8f6595da0b6cd70aab9e4f81e6d7727666
jose <josecarlosesparza@gmail.com>
2019-09-04 19:53:34 +0200

    Add specific show view for notes

--- FrontEnd/FinalProject/routes/notes.js
+++ FrontEnd/FinalProject/routes/notes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Note = require('../models/note');
 
+//INDEX - Show all the notes
 router.get("/", function (req, res) {
     Note.find({}, function (err, allNotes) {
         if (err) {
@@ -12,10 +13,25 @@ router.get("/", function (req, res) {
     });
 });
 
+//SHOW - Show page of each note, show more info about that note
+router.get("/:id", function(req, res){
+    //Find note with provided ID
+    Note.findById(req.params.id, function(err, foundNote){
+        if(err){
+            console.log(err);
+        } else{
+            // Render show template
+            res.render("../views/notes/show", {note: foundNote});
+        }
+    });
+});
+
+//NEW - Show form to create a new note
 router.get("/new", function (req, res) {
     res.render("notes/new")
 });
 
+//CREATE - Add new note to the DB
 router.post("/new", function (req, res) {
     var note = req.body.note;
 
@@ -33,4 +49,5 @@ router.post("/new", function (req, res) {
 
 });
 
+//
 module.exports = router;
