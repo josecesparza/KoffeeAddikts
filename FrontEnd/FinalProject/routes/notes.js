1b03b0d107e6eab98707c441185538944f702a8f
jose <josecarlosesparza@gmail.com>
2019-09-04 17:41:18 +0200

    Basic setup notes page

--- FrontEnd/FinalProject/routes/notes.js
+++ FrontEnd/FinalProject/routes/notes.js
@@ -2,7 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req, res){
-    res.render("notes/index");
+    var notes = [
+        {
+            title: "Note 1",
+            date: "22-12-2019",
+            content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati dolorum sint veritatis quod architecto quibusdam.",
+            Author: "Jose"  
+        },
+        {
+            title: "Note 2",
+            date: "02-12-2018",
+            content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati dolorum sint veritatis quod architecto quibusdam.",
+            Author: "Max"  
+        }
+    ];
+
+    res.render("notes/index", {notes: notes});
 });
 
 module.exports = router;
