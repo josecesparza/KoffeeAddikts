5086837957854eb3efeb569929598dd4322479a5
jose <josecarlosesparza@gmail.com>
2019-09-04 16:45:00 +0200

    Create notes page and route

--- FrontEnd/FinalProject/app.js
+++ FrontEnd/FinalProject/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 
+//Require Routes
+var notesRoutes = require('./routes/notes');
+
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("index");
 });
+
+app.use("/notes", notesRoutes);
 
 app.listen(3000, process.env.IP, function(){
     console.log("Server has started!");
