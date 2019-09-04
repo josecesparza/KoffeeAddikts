3a748b1be11156e281b6b1917a61e9f28b6c4852
jose <josecarlosesparza@gmail.com>
2019-09-04 18:24:25 +0200

    Add new note

--- FrontEnd/FinalProject/routes/notes.js
+++ FrontEnd/FinalProject/routes/notes.js
@@ -1,23 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/", function(req, res){
-    var notes = [
-        {
-            title: "Note 1",
-            date: "22-12-2019",
-            content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati dolorum sint veritatis quod architecto quibusdam.",
-            Author: "Jose"  
-        },
-        {
-            title: "Note 2",
-            date: "02-12-2018",
-            content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati dolorum sint veritatis quod architecto quibusdam.",
-            Author: "Max"  
-        }
-    ];
+var notes = [
+    {
+        title: "Note 1",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos deserunt, inventore pariatur voluptatibus laudantium in."
+    },
+    {
+        title: "Note 1",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos deserunt, inventore pariatur voluptatibus laudantium in."
+    }
+];
 
+router.get("/", function(req, res){
     res.render("notes/index", {notes: notes});
+});
+
+router.get("/new", function(req, res){
+    res.render("notes/new")
+});
+
+router.post("/new", function(req, res){
+    var newNote = req.body.note;
+
+    console.log(newNote);
+    notes.push(newNote);
+    res.redirect("/notes");
 });
 
 module.exports = router;
