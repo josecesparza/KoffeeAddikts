bdcbb26ee3d5f6ef6580b59b7960c8fcdd53df22
jose <josecarlosesparza@gmail.com>
2019-10-02 11:17:01 +0200

    Refactorization

--- FrontEnd/FinalProject/app.js
+++ FrontEnd/FinalProject/app.js
@@ -12,6 +12,8 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine", "ejs");
+
 //PASSPORT CONFIG
 app.use(require('express-session')({
     secret: "This is the Secret Page Test",
@@ -33,59 +35,15 @@ app.use(function(req, res, next){
 
 //Require Routes
 var notesRoutes = require('./routes/notes');
+var usersRoutes = require('./routes/users')
 
-app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("index");
 });
 
+app.use("/", usersRoutes);
 app.use("/notes", notesRoutes);
-
-//========================
-//AUTH ROUTES
-//========================
-//Show register form
-app.get("/register", function(req, res){
-    res.render("users/register");
-});
-
-//Handling sign up logic 
-app.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            return res.render("users/register");
-        }
-
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/notes");
-        });
-    });
-});
-
-//SHOW LOGIN FORM
-app.get("/login", function (req, res) {
-    res.render("users/login");
-});
-
-//Handling login logic
-//app.post("/login", middleware, callback);
-//When we call passport.authenticate we're calling a method using passport-local-mongoose
-app.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/notes",
-        failureRedirect: "/login"
-    }), function (req, res) {
-    }
-);
-
-//logout route
-app.get("/logout", function (req, res) {
-    req.logOut();
-    res.redirect("/notes");
-});
 
 app.listen(3000, process.env.IP, function(){
     console.log("Server has started!");
--- FrontEnd/FinalProject/routes/users.js
+++ FrontEnd/FinalProject/routes/users.js
@@ -1,5 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var User = require('../models/user');
+
+//Show register form
+router.get("/register", function(req, res){
+    res.render("users/register");
+});
+
+//Handling sign up logic 
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("users/register");
+        }
+
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/notes");
+        });
+    });
+});
+
+//SHOW LOGIN FORM
+router.get("/login", function (req, res) {
+    res.render("users/login");
+});
+
+//Handling login logic
+router.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/notes",
+        failureRedirect: "/login"
+    }), function (req, res) {
+    }
+);
+
+//logout route
+router.get("/logout", function (req, res) {
+    req.logOut();
+    res.redirect("/notes");
+});
 
 module.exports = router;
